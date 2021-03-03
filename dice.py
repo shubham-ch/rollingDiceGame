f80707d97cbab71f93630de4a9460bdd9bfe5a3c
shubham-ch <shubhamchaurasia551@gmail.com>
2021-03-03 16:37:28 +0100

    first

---
+++ dice.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import intertools
+import random
+background_color = "#0D865D"
+root = Tk()
+root.geometry("900x720")
+
+root.configure(bg = background_color)
+root.mainloop()
